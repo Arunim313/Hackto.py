652d4f0079b9eab4aff0490a938e80c84883e07a
khushbubijawat <68491547+khushbubijawat@users.noreply.github.com>
2022-10-22 18:09:05 +0530

    Delete text to handwriting.py

--- text to handwriting.py
+++
@@ -1,8 +0,0 @@
-import pywhatkit as kit
-import cv2
-
-kit.text_to_handwriting("Hope you are doing well", save_to="handwriting.png")
-img = cv2.imread("handwriting.png")
-cv2.imshow("Text to Handwriting", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
