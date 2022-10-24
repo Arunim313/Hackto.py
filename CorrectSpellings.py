3237a681933e7255e9da1ab9bbf763ad017dc24d
JainDevanshi <91427624+JainDevanshi@users.noreply.github.com>
2022-10-24 15:03:54 +0530

    Correct Spellings with Python

---
+++ CorrectSpellings.py
@@ -0,0 +1,9 @@
+from textblob import TextBlob
+words = ["Machne", "Learnin"]
+corrected_words = []
+for i in words:
+    corrected_words.append(TextBlob(i))
+print("Wrong words :", words)
+print("Corrected Words are :")
+for i in corrected_words:
+    print(i.correct(), end=" ")
