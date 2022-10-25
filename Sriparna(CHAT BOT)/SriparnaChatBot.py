1396d3ac69b32266399e04f9bc04b1b6a3e20180
Soumyajit2005 <soumyajitparia2005@gmail.com>
2022-10-26 00:10:26 +0530

    Chatbot created using python

---
+++ Sriparna(CHAT BOT)/SriparnaChatBot.py
@@ -0,0 +1,74 @@
+import enum
+import random
+import json
+import pickle
+import numpy as np
+import time
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from six import class_types
+
+from tensorflow.keras.models import load_model
+
+lematizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lematizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+        return return_list
+
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+if __name__ == "__main__":
+
+    print("Go! Bot is running...")
+
+    while True:
+        message = input("Soumyajit: ")
+        if "Go offline" in message or "go offline" in message:
+            print("Going offline...")
+            break
+        else:
+            ints = predict_class(message)
+            res = get_response(ints, intents)
+            time.sleep(0.5)
+            print("Sriparna:", res)
