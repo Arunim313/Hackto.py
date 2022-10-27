1ee6764fb1dda9b5305dd25e82a174f99b257ae4
Huzaifa-code <huzaifaqureshi037@gmail.com>
2022-10-27 19:31:09 +0530

    Stack

--- stack.py
+++ stack.py
@@ -11,3 +11,11 @@ class Stack:
                     l = len(self.items)-1
                     return self.items[l]
         def size(self):                                                                             return len(self.items)
+
+stack = Stack()
+print(stack.is_empty()) 
+
+for i in range(0, 10):
+        stack.push(i)
+print(stack.size())
+print(stack.items)
