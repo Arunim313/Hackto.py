e2b6191c2092c8f441881a5710cbc336bab594ac
Aastha Mujawdiya <103181989+aastha2803@users.noreply.github.com>
2022-10-27 11:23:32 +0530

    add message

--- Alarm Clock Using Python/alarm.clock.py
+++ Alarm Clock Using Python/alarm.clock.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from playsound import playsound
 
 print("\n*************Hello Dude***************\n")
+print("Would you Please,")
 alarm_time = input("Enter the time of alarm to be set:HH:MM:SS : \t")
 alarm_hour=alarm_time[0:2]
 alarm_minute=alarm_time[3:5]
